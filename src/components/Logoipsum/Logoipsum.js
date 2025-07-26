c4d6e9a4b7c6f31609a2f821f4540a38506f5294
Karandeep-BS <karandeepsinghbusiness@gmail.com>
2025-07-26 15:53:15 +0530

    1.1v

--- src/components/Logoipsum/Logoipsum.js
+++ src/components/Logoipsum/Logoipsum.js
@@ -1,20 +1,32 @@
+'use client'
+
 import Box1 from "./Box1"
 import Box2 from "./Box2"
 import Box3 from "./Box3"
 import Box4 from "./Box4"
 import BoxFirst from "./BoxFirst"
 import BoxLast from "./BoxLast"
-import "../Logoipsum/MobileBox/Boxes.css"
-// import Box5 from "./Box5"
-export default function Logoipsum(){
-    return(
-        <div className="logoipsum logoipsum-marquee h-[30vh] bg-white w-full relative  flex justify-between items-center ">
-            <BoxFirst />
-            <Box1 />
-            <Box2 />
-            <Box3 />
-            <Box4 />
-            <BoxLast />
-        </div>
-    )
+import "../Logoipsum/Animation.css"
+
+export default function Logoipsum() {
+  const boxComponents = [
+    <BoxFirst key="first" />,
+    <Box1 key="1" />,
+    <Box2 key="2" />,
+    <Box3 key="3" />,
+    <Box4 key="4" />,
+    <BoxLast key="last" />
+  ];
+
+  return (
+    <div className="D-logoipsum-container w-full h-[30vh] overflow-hidden flex justify-between items-center bg-[#fffefe] relative">
+      <div className="logoipsum D-logoipsum-marquee flex w-max ">
+        {[...boxComponents, ...boxComponents].map((Component, i) => (
+          <div key={i} className="flex justify-center items-center ml-[3vh] mr-[3vh] -bg-pink-400">
+            {Component}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
--- src/components/Logoipsum/MobileBox/MLogoipsum.js
+++ src/components/Logoipsum/MobileBox/MLogoipsum.js
@@ -1,57 +1,67 @@
+'use client'
 // import Box1 from "./Box1"
 // import Box2 from "./Box2"
 // import Box3 from "./Box3"
 // import Box4 from "./Box4"
 // import BoxFirst from "./BoxFirst"
 // import BoxLast from "./BoxLast"
-// import "../Logoipsum/MobileBox/Boxes.css"
 // import Box5 from "./Box5"
-export default function MLogoipsum(){
-    return(
-        // -----------------------------------------
-        <div className="logoipsum logoipsum-marquee h-[30vh]   mb-[2vh] w-[240vh] relative justify-between items-center  flex overflow-hidden ">
-            <div className=" flex justify-center items-center h-[20vh] w-[30vh]">
+import "../MobileBox/Boxes.css"
+
+export default function MLogoipsum() {
+  return (
+    <div className="logoipsum-container overflow-hidden  w-full relative">
+      <div className="logoipsum-marquee flex w-max  animate-marquee">
+        {/* =================== First Set =================== */}
+        {Array(2)
+          .fill(0)
+          .map((_, index) => (
+            <div key={index} className="flex justify-between w-[220vh] ">
+              <div className="flex justify-center items-center h-[20vh] w-[30vh]">
                 <img
                   src="/images/Akairis/Vector-First.svg"
                   alt="Akairis Vector"
                   className="Boximg w-[30vh] h-[10vh]"
                 />
-            </div>
-            <div className=" flex justify-center items-center h-[20vh] w-[30vh]">
+              </div>
+              <div className="flex justify-center items-center h-[20vh] w-[30vh]">
                 <img
                   src="/images/Akairis/Vector-1.svg"
                   alt="Akairis Vector"
                   className="Boximg w-[30vh] h-[10vh]"
                 />
-            </div>
-            <div className="flex justify-center items-center h-[20vh] w-[30vh]">
+              </div>
+              <div className="flex justify-center items-center h-[20vh] w-[30vh]">
                 <img
                   src="/images/Akairis/Vector-2.svg"
                   alt="Akairis Vector"
                   className="Boximg w-[30vh] h-[10vh]"
                 />
-            </div>
-            <div className=" flex justify-center items-center h-[20vh] w-[30vh]">
+              </div>
+              <div className="flex justify-center items-center h-[20vh] w-[30vh]">
                 <img
                   src="/images/Akairis/Vector-3.svg"
                   alt="Akairis Vector"
                   className="Boximg w-[30vh] h-[10vh]"
                 />
-            </div>
-            <div className=" flex justify-center items-center h-[20vh] w-[30vh]">
+              </div>
+              <div className="flex justify-center items-center h-[20vh] w-[30vh]">
                 <img
                   src="/images/Akairis/Vector-4.svg"
                   alt="Akairis Vector"
                   className="Boximg w-[30vh] h-[10vh]"
                 />
-            </div>
-            <div className=" flex justify-center items-center h-[20vh] w-[30vh]">
+              </div>
+              <div className="flex justify-center  mr-[6vh] items-center h-[20vh] w-[30vh]">
                 <img
                   src="/images/Akairis/Vector-Last.svg"
                   alt="Akairis Vector"
                   className="Boximg w-[30vh] h-[10vh]"
                 />
+              </div>
             </div>
-        </div>
-    )
+          ))}
+      </div>
+    </div>
+  )
 }
